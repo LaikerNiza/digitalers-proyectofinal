1a32e2ed347272008f5302759912b569c1885565
LaikerNiza <106284132+LaikerNiza@users.noreply.github.com>
2022-10-19 22:59:17 -0300

    hecho

---
+++ app.js
@@ -0,0 +1,25 @@
+// app.js
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+// motor de plantillas
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+app.use(express.static(__dirname + "/public"));
+
+//Rutas web
+app.use('/', require('./router/articles'));
+
+app.use((req, res, next) => {
+    res.status(404).render("404", {
+        titulo: "404",
+        descripcion: "Título del sitio web"
+    })
+})
+
+app.listen(port, () => {
+    console.log('servidor a su servicio en el puerto', port)
+})
